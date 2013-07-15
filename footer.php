4f904f83e5559dbb4e3da2f4cdfce2ce3a51e682
Ali aGHasizadeh <me@aligh.pro>
2013-07-15 22:22:25 +0430

    add footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+</div>
+<section class="slideshow">
+  <div class="noise"></div>
+  <section class="background">
+    <div style="background-image:url('<?php bloginfo('template_url'); ?>/images/backgrounds/bg1.jpg');"></div>
+    <div style="background-image:url('<?php bloginfo('template_url'); ?>/images/backgrounds/bg2.jpg');"></div>
+    <div style="background-image:url('<?php bloginfo('template_url'); ?>/images/backgrounds/bg3.jpg');"></div>
+    <div style="background-image:url('<?php bloginfo('template_url'); ?>/images/backgrounds/bg4.jpg');"></div>
+    <div style="background-image:url('<?php bloginfo('template_url'); ?>/images/backgrounds/bg5.jpg');"></div>
+  </section>
+</section>
+
+<script type="text/javascript">document.write('<script type="text/javascript" src=<?php bloginfo("template_url"); ?>/scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');</script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/scripts/underdev.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/scripts/script.js"></script>
+</body>
+</html>
