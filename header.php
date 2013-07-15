96442c99e5fae1166124ca9985d321192860eab1
Ali aGHasizadeh <me@aligh.pro>
2013-07-15 22:22:32 +0430

    add header.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8" />
+  <title><?php bloginfo('name'); wp_title(); ?></title>
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/1styles.css" />
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css" />
+  <link href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700" rel="stylesheet" type="text/css" />
+  <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/scripts/html5shiv.js"></script>
+</head>
+<body>
+  <div class="w24 mlra container mh40">
