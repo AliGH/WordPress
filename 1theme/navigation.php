083fb8b11222e98a82f299a2bb3bafd2bc364aa6
Ali aGHasizadeh <me@aligh.pro>
2013-07-23 18:47:08 +0430

    fix category for menus

--- 1theme/navigation.php
+++ 1theme/navigation.php
@@ -1,6 +1,16 @@
 <!-- Le Header
 ================================================== -->
 <!-- This is the logo and navigation -->
+<?php
+
+  $all_categories = get_categories( $args );
+  $args = array(
+    'type' => 'post',
+    'orderby' => 'name',
+    'order' => 'ASC',
+    'hide_empty' => True
+  );
+?>
 
 	<div class="navigation">
 		<nav>
@@ -14,19 +24,18 @@
 			================================================== -->
 			<!-- Navigation begins here -->
 			<?php
-          wp_nav_menu(array(
-            'theme_location'  => 'mainmenu',
-            'container'       => 'div', 
-            'container_class' => 'collapse',
-            'container_id'    => 'nav-collapse',
-            'menu_class'      => 'nav', 
+          // wp_nav_menu(array(
+          //   'theme_location'  => 'mainmenu',
+          //   'container'       => 'div', 
+          //   'container_class' => 'collapse',
+          //   'container_id'    => 'nav-collapse',
+          //   'menu_class'      => 'nav', 
            // 'before'          => '',
            // 'after'           => '<span>describe the page</span>'
            // 'link_before'     => '',
            // 'link_after'      => ''
-          ));
+          //));
         ?>
-        <!-- 
 			<div id="nav-collapse" class="collapse">
 				<ul class="nav">
 				
@@ -35,9 +44,16 @@
 					<li>
 						<a href="#filter=.portfolio" class="selected"><span data-toggle="collapse" data-target="#portfolio-collapse"></span>نمونه کارها</a>
 						<ul id="portfolio-collapse" class="collapse out">
-							<li><a href="#filter=.graphics">گرافیک</a></li>
+              <?php
+                foreach($all_categories as $category) {
+              ?>
+                  <li><a href="#filter=.<?php echo $category->slug; ?>"><?php echo $category->name; ?></a></li>
+              <?php
+               } 
+              ?>
+							<!-- <li><a href="#filter=.graphics">گرافیک</a></li>
 							<li><a href="#filter=.illustration">وب سایت</a></li>
-							<li><a href="#filter=.web">موبایل</a></li>
+							<li><a href="#filter=.web">موبایل</a></li> -->
 						</ul>
 					</li>
 					
@@ -61,7 +77,7 @@
 					</li>
 					
 				</ul>
-			</div> -->
+			</div>
 		</nav>	 
 		<div class="nav-arrow">
 		</div>
