3727ba73f488eae5468b23895d0d3fb777800ca7
Ali aGHasizadeh <me@aligh.pro>
2013-07-26 10:25:53 +0430

    add pr footer

---
+++ 1theme/pr-footer.php
@@ -0,0 +1,83 @@
+
+  
+  <!-- Footer
+  ================================================== -->
+  <!-- Copyright & Social Buttons -->
+  
+   <div class="footer">
+    <ul>
+      <li class="right">© Copyright 2013 <a href="http://1dws.com">1Devs</a></li>
+
+      <li class="social-button left">
+    <a rel="alternate" data-original-title="Facebook">
+          <img src="img/social-icons/glyphicons_410_facebook.png" alt="" />
+        </a>
+      </li>
+      <li class="social-button left">
+        <a rel="alternate" data-original-title="Google+">
+          <img src="img/social-icons/glyphicons_382_google_plus.png" alt="" />
+        </a>
+      </li>
+      <li class="social-button left">
+        <a rel="alternate" data-original-title="Twitter">
+          <img src="img/social-icons/glyphicons_411_twitter.png" alt="" />
+        </a>
+      </li>
+      <li class="social-button left">
+        <a rel="alternate" data-original-title="Vimeo">
+          <img src="img/social-icons/glyphicons_414_vimeo.png" alt="" />
+        </a>
+      </li>
+      <li class="social-button left">
+        <a rel="alternate" data-original-title="LinkedIn">
+          <img src="img/social-icons/glyphicons_397_linked_in.png" alt="" />
+        </a>
+      </li>
+      <li class="social-button left">
+        <a rel="alternate" data-original-title="Dribble">
+          <img src="img/social-icons/glyphicons_394_dribbble.png" alt="" />
+        </a>
+      </li>
+
+    </ul>
+   </div>
+</div>
+  
+  <!-- JQUERY (flexslider)
+  ================================================== -->
+  <!-- Script for the awesome slider -->
+  
+  <script type="text/javascript">
+    $(window).load(function() {
+      $('.flexslider').flexslider({
+        animation: "slide",
+      
+      });
+      
+    });
+  </script>
+  
+  <!-- JQUERY (tooltips)
+  ================================================== -->
+  <!-- Script for the awesome tooltips -->
+  
+  <script>
+  $("[rel=alternate]").tooltip()
+  </script>
+
+
+    <script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = '1tajrobe'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function () {
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
+    </script>
+    
+</body>
+</html>
